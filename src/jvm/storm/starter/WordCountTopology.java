7a5179e60b9dac515f4f1408e885d1f765245310
Karthik Ramasamy <kramasamy@twitter.com>
2014-01-09 10:53:47 -0800

    Removed python & ruby dependency in WordCount

--- src/jvm/storm/starter/WordCountTopology.java
+++ src/jvm/storm/starter/WordCountTopology.java
@@ -4,57 +4,87 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
 import backtype.storm.task.ShellBolt;
-import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import storm.starter.spout.RandomSentenceSpout;
 
+import backtype.storm.utils.Utils;
+
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.OutputCollector;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * This topology demonstrates Storm's stream groupings and multilang capabilities.
  */
 public class WordCountTopology {
-  public static class SplitSentence extends ShellBolt implements IRichBolt {
 
-    public SplitSentence() {
-      super("python", "splitsentence.py");
+  private WordCountTopology() { }
+
+  /**
+   * A spout that emits a random word
+   */
+  static class WordSpout extends BaseRichSpout {
+    private Random rnd;
+    private SpoutOutputCollector collector;
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+      outputFieldsDeclarer.declare(new Fields("word"));
     }
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word"));
+    public void open(Map map, TopologyContext topologyContext,
+                     SpoutOutputCollector spoutOutputCollector) {
+      rnd = new Random(31);
+      collector = spoutOutputCollector;
     }
 
     @Override
-    public Map<String, Object> getComponentConfiguration() {
-      return null;
+    public void nextTuple() {
+      String[] list = {"Jack", "Mary", "Jill", "McDonald"};
+      Utils.sleep(1000);
+      int nextInt = rnd.nextInt(list.length);
+      collector.emit(new Values(list[nextInt]));
     }
   }
 
-  public static class WordCount extends BaseBasicBolt {
-    Map<String, Integer> counts = new HashMap<String, Integer>();
-
-    @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
-      String word = tuple.getString(0);
-      Integer count = counts.get(word);
-      if (count == null)
-        count = 0;
-      count++;
-      counts.put(word, count);
-      collector.emit(new Values(word, count));
+  /**
+   * A bolt that counts the words that it receives
+   */
+  static class ConsumerBolt extends BaseRichBolt {
+    private OutputCollector collector;
+    private Map<String, Integer> countMap;
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+      collector = outputCollector;
+      countMap = new HashMap<String, Integer>();
     }
 
     @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-      declarer.declare(new Fields("word", "count"));
+    public void execute(Tuple tuple) {
+      String key = tuple.getString(0);
+      if (countMap.get(key) == null) {
+        countMap.put(key, 1);
+      } else {
+        Integer val = countMap.get(key);
+        countMap.put(key, ++val);
+      }
+      collector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
     }
   }
 
@@ -62,10 +92,10 @@ public class WordCountTopology {
 
     TopologyBuilder builder = new TopologyBuilder();
 
-    builder.setSpout("spout", new RandomSentenceSpout(), 5);
+    builder.setSpout("spout", new WordSpout(), 5);
 
-    builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
-    builder.setBolt("count", new WordCount(), 12).fieldsGrouping("split", new Fields("word"));
+    // builder.setBolt("split", new SplitSentence(), 8).shuffleGrouping("spout");
+    builder.setBolt("count", new ConsumerBolt(), 15).fieldsGrouping("spout", new Fields("word"));
 
     Config conf = new Config();
     conf.setDebug(true);
